d5e9cd9f5e917f7ff0db99d699aacb394608a4fa
lillian <keynesiane@gmail.com>
2024-01-26 12:54:20 -0800

    have form and html reflect each other for top part

--- cv-app/src/TopForm.jsx
+++ cv-app/src/TopForm.jsx
@@ -9,6 +9,9 @@ import { useState } from 'react'
 function TopForm() {
 
   const[showForm, setShowForm] = useState(true)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
 
     return (
       <div>
@@ -17,24 +20,31 @@ function TopForm() {
         {showForm && <Form id="genInfo">
         <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name" />
+        <Form.Control type="text" placeholder="Name" value={name}
+        onChange={(event) => setName(event.target.value)}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" />
+        <Form.Control type="email" placeholder="Email" value={email}
+        onChange={(event) => setEmail(event.target.value)}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPhone">
         <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="tel" placeholder="Phone Number" />
+        <Form.Control type="tel" placeholder="Phone Number" value={phone}
+        onChange={(event) => setPhone(event.target.value)}/>
       </Form.Group>
       </Form>}
+
       {!showForm && 
       <div>
-      <h3>Name</h3>
-      <h3>Email</h3>
-      <h3>Phone Number</h3>
+      <h4>Name</h4>
+      <p>{name}</p>
+      <h4>Email</h4>
+      <p>{email}</p>
+      <h4>Phone Number</h4>
+      <p>{phone}</p>
       </div>}
     
           <ButtonGroup aria-label="change buttons">
